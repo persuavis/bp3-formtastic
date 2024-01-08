576a1b0e70316024bc6e31a6fc4628d8b3851c03
Wim den Braven <wimdenbraven@persuavis.com>
2024-01-08 15:34:22 -0800

    Initial commit

---
+++ lib/bp3/formtastic.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'formtastic/version'
+
+module Bp3
+  module Formtastic
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
