e2367ce9a56090d08f88d308f03602ad80142569
Wim den Braven <wimdenbraven@persuavis.com>
2024-08-20 14:58:06 -0700

    Allow nil input_control_class_name

--- lib/bp3/formtastic.rb
+++ lib/bp3/formtastic.rb
@@ -3,6 +3,8 @@
 require 'active_support/core_ext/module/attribute_accessors'
 require 'active_support/inflector'
 
+require_relative 'formtastic/viz_form_builder'
+require_relative 'formtastic/viz_form_builder_with_create'
 require_relative 'formtastic/railtie'
 require_relative 'formtastic/version'
 
@@ -11,7 +13,7 @@ module Bp3
     mattr_accessor :input_control_class_name
 
     def self.input_control_class
-      @@input_control_class ||= input_control_class_name.constantize # rubocop:disable Style/ClassVars
+      @@input_control_class ||= input_control_class_name&.constantize # rubocop:disable Style/ClassVars
     end
   end
 end
